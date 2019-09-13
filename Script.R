c85bd3d8dc552c9cb3340acb3070bc4a2ff1c149
Eveline Mariz <evejbm@gmail.com>
2019-09-13 00:11:29 -0300

    Atualiza script

--- Script.R
+++ Script.R
@@ -2,12 +2,14 @@ rm(list=ls())
 cat("\014")
 library("readr")
 library("tidyverse")
-dados2 <- read_delim("MICRODADOS_100.txt", ";", escape_double = FALSE, 
+dados <- read_delim("MICRODADOS_2018.txt", ";", escape_double = FALSE, 
                       locale = locale(encoding = "ISO-8859-1"), 
                              trim_ws = TRUE)
-#dados <- dados[c(-2,-3,-5,-12:-123)]
-dados3 <- dados2 %>% select(-2,-3,-5,-12:-80)
-dados4 <- dados3 %>% select(-23:-32)
-dados <- dados4 %>% select(-9:-14,-29:-55)
-rm(dados2,dados3,dados4)
+dados <- dados[c(-2,-3,-5,-12:-15,-19,-21:-82,-87:-90,-95:-98,-100:-103)]
+#dados3 <- dados2 %>% select(-2,-3,-5,-12:-80)
+#dados4 <- dados3 %>% select(-23:-32)
+#dados <- dados4 %>% select(-9:-14,-29:-55)
+#rm(dados2,dados3,dados4)
+#var(dados[,17],na.rm = TRUE)
 View(dados)
+
